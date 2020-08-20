f66488cd2251e35caa2b4f2580479e1bc3fd1fbb
Evgenii-Nazarov <ev66647260@gmail.com>
2020-08-20 16:34:45 -0700

    Updates

---
+++ precache-manifest.6b18a691d58477bd13e6445ebe85a5c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bac7f7d1fa364a495cc3837406d2b41",
+    "url": "/axios-connection/index.html"
+  },
+  {
+    "revision": "e6d782726cb5075e79cf",
+    "url": "/axios-connection/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "923dc928250be58614ee",
+    "url": "/axios-connection/static/js/2.1626eb19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/axios-connection/static/js/2.1626eb19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6d782726cb5075e79cf",
+    "url": "/axios-connection/static/js/main.59ee5b20.chunk.js"
+  },
+  {
+    "revision": "aaf89ba28bce47eb97f2",
+    "url": "/axios-connection/static/js/runtime-main.ff676d91.js"
+  }
+]);
